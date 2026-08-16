04ffde0d0e7f4aa2778c309c3260676f94c7b65b
pratiklohar14 <pratiklohar0712@gmail.com>
2026-08-17 00:43:19 +0530

    initial project

---
+++ demo/src/main/java/com/example/Main.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import com.example.view.Welcome;
+
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(Welcome.class, args);
+        
+    }
+}
